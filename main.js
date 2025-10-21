3a26a8f4d5e8492adc9da1af79e13cd320fe2e31
HarrBear37 <hdb5168@psu.edu>
2025-10-21 13:00:35 -0400

    Updates

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/loaders/GLTFLoader.js';
 
 // Setup
 
@@ -21,6 +20,17 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(50);
 camera.position.setX(-3);
+
+// Lights
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(0, -10, 10);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+ambientLight.position.set(25, -15, -400);
+
+scene.add(pointLight);
+scene.add(ambientLight);
 
 const geometry = new THREE.BoxGeometry(10, 10, 10);
 
@@ -64,34 +74,6 @@ const torusKnot = new THREE.Mesh( torusGeo, torusMaterial );
 scene.add( torusKnot );
 torusKnot.position.y = 20
 
-// Lights
-
-const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(0, -10, 10);
-
-const ambientLight = new THREE.AmbientLight(0xffffff);
-ambientLight.position.set(25, -15, -400);
-
-scene.add(pointLight);
-scene.add(ambientLight);
-
-function animate() {
-    requestAnimationFrame( animate );
-    // slowly rotate the cube:
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    // rotate the icosahedron a little faster in the opposite direction:
-    icoMesh.rotation.z += -0.03;
-    icoMesh.rotation.y += -0.03;
-
-    //smileMesh.rotation.y += -0.05;
-    
-
-
-    renderer.render( scene, camera );
-}
-animate();
-
 // Helpers
 
 const lightHelper = new THREE.PointLightHelper(pointLight);
@@ -123,3 +105,89 @@ const smileMaterial = new THREE.MeshStandardMaterial({map: smileTexture})
 const smileMesh = new THREE.Mesh(sphereGeometry, smileMaterial);
 
 scene.add(smileMesh);
+
+smileMesh.position.y = 10;
+smileMesh.rotation.y = Math.PI / -2;
+
+//body
+
+const bod = new THREE.BoxGeometry(10, 20, 10);
+
+const bodMaterial = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+
+const prismMesh = new THREE.Mesh(bod, bodMaterial);
+
+scene.add(prismMesh);
+
+prismMesh.position.y = -10;
+
+//Right arm
+
+const armR = new THREE.BoxGeometry(2.5, 15, 2.5);
+
+const armRMaterial = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+
+const armRMesh = new THREE.Mesh(armR, armRMaterial);
+
+scene.add(armRMesh);
+
+armRMesh.position.x = 9;
+armRMesh.position.y = -7;
+armRMesh.rotation.z = Math.PI / 1.35;
+
+//left arm
+
+const armL = new THREE.BoxGeometry(2.5, 15, 2.5);
+
+const armLMaterial = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+
+const armLMesh = new THREE.Mesh(armL, armLMaterial);
+
+scene.add(armLMesh);
+
+armLMesh.position.x = -9;
+armLMesh.position.y = -7;
+armLMesh.rotation.z = Math.PI / -1.35;
+
+//right leg
+
+const legR = new THREE.BoxGeometry(2.5, 15, 2.5);
+
+const legRMaterial = new THREE.MeshStandardMaterial({ color: 0x0000ff });
+
+const legRMesh = new THREE.Mesh(legR, legRMaterial);
+
+scene.add(legRMesh);
+
+legRMesh.position.x = 2;
+legRMesh.position.y = -25;
+
+//left leg
+
+const legL = new THREE.BoxGeometry(2.5, 15, 2.5);
+
+const legLMaterial = new THREE.MeshStandardMaterial({ color: 0x0000ff });
+
+const legLMesh = new THREE.Mesh(legL, legLMaterial);
+
+scene.add(legLMesh);
+
+legLMesh.position.x = -2;
+legLMesh.position.y = -25;
+
+function animate() {
+    requestAnimationFrame( animate );
+    // slowly rotate the cube:
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    // rotate the icosahedron a little faster in the opposite direction:
+    icoMesh.rotation.z += -0.03;
+    icoMesh.rotation.y += -0.03;
+
+    //smileMesh.rotation.y += -0.05;
+    controls.update();
+
+
+    renderer.render( scene, camera );
+}
+animate();
